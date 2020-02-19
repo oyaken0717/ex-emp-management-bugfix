cbb02a3a5ff18fa7e5ea35d7e700561197c5b84c
KenjiOyamada <oyaken0717@yahoo.co.jp>
2020-02-19 16:21:59 +0900

    【modify】(1-4)初級 ダブルサブミット対策

--- src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
@@ -80,8 +80,7 @@ public class AdministratorController {
 		// フォームからドメインにプロパティ値をコピー
 		BeanUtils.copyProperties(form, administrator);
 		administratorService.insert(administrator);
-//×		return "employee/list";
-		return "administrator/login";
+        return "redirect:/";
 	}
 
 	/////////////////////////////////////////////////////
