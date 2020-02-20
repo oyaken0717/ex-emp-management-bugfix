138fc6095ae70c6089bb0f2be0b21fb669cbb225
KenjiOyamada <oyaken0717@yahoo.co.jp>
2020-02-20 14:34:10 +0900

    【add】(2-1)中級 ログイン者名表示

--- src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/emp_management/controller/AdministratorController.java
@@ -120,6 +120,8 @@ public class AdministratorController {
 			model.addAttribute("errorMessage", "メールアドレスまたはパスワードが不正です。");
 			return toLogin();
 		}
+		session.setAttribute("administratorName",administrator.getName());
+//		model.addAttribute("administratorName",administrator.getName());
 		return "forward:/employee/showList";
 	}
 
